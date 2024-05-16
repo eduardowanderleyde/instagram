b47963498d4b1d5cb23d99f7d506022d92010160
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-05-16 18:43:51 -0300

    Adding Plugin to only accepted image file

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,13 +1,22 @@
 import * as FilePond from "filepond";
 
+// Import the plugins
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 
-FilePond.registerPlugin(FilePondPluginImagePreview);
+// Register the plugins
+FilePond.registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginImageType
+  );
 
+// Get a reference to the file input element
 const inputElement = document.querySelector('#post-images');
 
+// Create a FilePond instance
 const pond = FilePond.create(inputElement, {
   credits: false,
   allowMultiple: true,
   allowReorder: true,
+  acceptedFileTypes: ['image/*'],
 });
