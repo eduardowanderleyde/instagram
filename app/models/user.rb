bc54fc59191dce73368ee91858ec19f954b7dde1
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-05-28 18:39:35 -0300

    Follow System Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ApplicationRecord
   has_many :posts
   has_one_attached :profile_pic  
   has_many :likes
-
   has_many :comments
+
+  has_many :followed_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "followed_id"
+  has_many :accepted_recieved_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "followed_id"
+  has_many :accepted_sent_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "follower_id"
+
+  #has_many :recieved_requests, class_name: "Follow", foreign_key: "followed_id"
+  #has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
+
+  #has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
 end
--- db/migrate/20240528211748_create_follows.rb
+++ db/migrate/20240528211748_create_follows.rb
@@ -1,10 +1,15 @@
 class CreateFollows < ActiveRecord::Migration[7.1]
   def change
     create_table :follows do |t|
-      t.integer :follower_id
-      t.boolean :accepted
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
+      t.boolean :accepted, default: false
 
       t.timestamps
     end
+
+    add_index :follows, :follower_id
+    add_index :follows, :followed_id
+    add_index :follows, [:follower_id, :followed_id], unique: true
   end
 end
