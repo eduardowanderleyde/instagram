72e412304ced0ada0ce68209416ec07dad8e7e79
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-11-19 12:32:53 -0300

    enviroment variables errors

--- db/migrate/20240517173308_create_likes.rb
+++ db/migrate/20240517173308_create_likes.rb
@@ -4,7 +4,7 @@ class CreateLikes < ActiveRecord::Migration[7.1]
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
 
-      t.index[:user_id, :post_id], unique: true
+      t.index [:user_id, :post_id], unique: true 
       t.timestamps
     end
   end
